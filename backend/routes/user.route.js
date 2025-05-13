e014e48fb4967ec47bad726c29b8dabb23138244
alishabohara03 <alishabohara369@gmail.com>
2025-05-13 22:27:41 +0545

    user controller and router

---
+++ backend/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {editProfile, followOrUnfollow, getprofile, getSuggestedUsers, login, logout, register} from "../controllers/user.controller.js"
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
+const router = express.Router();
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').post(logout);
+router.route('/:id/profile').post(isAuthenticated,getprofile);
+router.route('/profile/edit').post(isAuthenticated,upload.single('profilePicture'), editProfile);
+router.route('/suggested').get(isAuthenticated, getSuggestedUsers);
+router.route('/followorunfollow/:id').post(isAuthenticated, followOrUnfollow);
+export default router;
